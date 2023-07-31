a26607881267f5be9a596cece859539c7509dc69
vier21 <pilyaixavier21@gmail.com>
2023-07-31 16:58:50 +0700

    add module

---
+++ bin/cmd/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/jmoiron/sqlx"
+)
+
+func main() {
+
+	db, err := sqlx.Connect("mysql", "root@tcp(127.0.0.1:3306)/coba")
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+
+}
