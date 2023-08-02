93404bf61477d23c742d355102345765d6f09b9f
vier21 <pilyaixavier21@gmail.com>
2023-08-03 04:28:34 +0700

    adjust project layout

--- bin/cmd/main.go
+++ bin/cmd/main.go
@@ -9,10 +9,16 @@ import (
 
 func main() {
 
-	db, err := sqlx.Connect("mysql", "root@tcp(127.0.0.1:3306)/coba")
+	db, err := sqlx.Connect("mysql", "root@tcp(127.0.0.1:3306)/" + "coba")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+	p := []string{}
 
+	db.Select(&p, "SHOW TABLES")
+	for i := range p {
+
+		log.Println(p[i])
+	}
 }
